0f9f1fbf70dc9100fb1793d849548ca68b2c5e58
github username <github email address>
2020-01-16 11:19:39 -0700

    added end of input error fix to users controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,16 +4,16 @@ class AuthenticationController < ApplicationController
         password = params[:user][:password]
 
         @user = User.find_by(username: username)
-
+        
         if !@user
             render status: :unauthorized
         else 
             if !@user.authenticate(password)
-                secret_key = Rails.application.secrets.secret_key_base[0]
-                token = JWT.encode(@user, secret_key)
-                render json: {token: token}
-            else 
                 render status: :unauthorized
+            else 
+                secret_key = Rails.application.secrets.secret_key_base
+                token = JWT.encode({user_id: @user.id}, secret_key)
+                render json: {token: token}
             end
         end
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
+class JsonWebToken
+    
+    SECRET = Rails.application.secrets.secret_key_base.to_s
+    def self.encode(payload)
+        payload[:exp] = 24.hours.from_now.to_i
+        JWT.encode(payload, SECRET)
+    end
+
+    def self.decode(token)
+        JWT.decode(token, SECRET)[0]
+    end
+
+end
 class UsersController < ApplicationController
      # before_action :verify_user, except: [:create, :index]
      before_action :find_user, only: [:update]
@@ -22,10 +35,21 @@ class UsersController < ApplicationController
  
          if (@user.id == payload["user_id"].to_i)
              @user.update(user_params)
-             render json: {user_id: @user.id}
+             render json: {user: @user}
          else
              render json: {error: "Nice Try"}, status: :unauthorized 
          end
     end
+
+
+    private
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit([:username, :password, :total_games, :total_sharks_killed, :total_points])
+    end
  
 end
+
